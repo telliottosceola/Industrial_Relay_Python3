c2dc14f42bcb69f3ed344359701b07b2987812d2
telliottosceola <travis@controlanything.com>
2020-11-04 13:21:21 -0600

    initial commit

---
+++ Serial-Port-Relay-Example.py
@@ -0,0 +1,15 @@
+#import the pyserial module
+import serial
+import ncd_industrial_relay
+import time
+
+
+#set up your serial port with the desire COM port and baudrate.
+serial_port = serial.Serial('/dev/tty.usbserial-AG0JV64T', baudrate=115200, bytesize=8, stopbits=1, timeout=.5)
+#instantiate the board object and pass it the serial port
+board1 = ncd_industrial_relay.Relay_Controller(serial_port)
+
+board1.turn_on_relay_by_index(1)
+time.sleep(2)
+board1.turn_off_relay_by_index(1)
+serial_port.close()
