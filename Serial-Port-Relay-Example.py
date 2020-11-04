1026bfd0c0c04995f1bf9dc6e1624f97243f613d
telliottosceola <travis@controlanything.com>
2020-11-04 16:33:54 -0600

    added print out for status

--- Serial-Port-Relay-Example.py
+++ Serial-Port-Relay-Example.py
@@ -10,6 +10,15 @@ serial_port = serial.Serial('/dev/tty.usbserial-AG0JV64T', baudrate=115200, byte
 board1 = ncd_industrial_relay.Relay_Controller(serial_port)
 
 board1.turn_on_relay_by_index(1)
+
+response = board1.get_relay_status_by_index(1)
+print(response[0])
+
 time.sleep(2)
+
 board1.turn_off_relay_by_index(1)
+
+response = board1.get_relay_status_by_index(1)
+print(response[0])
+
 serial_port.close()
